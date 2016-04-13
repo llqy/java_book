15bc1ab01e9082d341a865e3c0200e1903bcff7a
yalong <lvxiaolu163@163.com>
2016-04-13 08:32:42 +0800

    # WARNING: head commit changed in the meantime
    test

--- src/Chapter12/EnumDemo2.java
+++ src/Chapter12/EnumDemo2.java
@@ -4,25 +4,25 @@ package Chapter12;
 //Use the built-in enumeration methods. 
 
 //An enumeration of apple varieties. 
-enum Apple {
+enum Apple22 {
 	Jonathan, GoldenDel, RedDel, Winesap, Cortland
 }
 
 class EnumDemo2 {
 	public static void main(String args[]) {
-		Apple ap;
+		Apple22 ap;
 
 		System.out.println("Here are all Apple constants");
 
 		// use values()
-		Apple allapples[] = Apple.values();
-		for (Apple a : allapples)
+		Apple22 allapples[] = Apple22.values();
+		for (Apple22 a : allapples)
 			System.out.println(a);
 
 		System.out.println();
 
 		// use valueOf()
-		ap = Apple.valueOf("Winesap");
+		ap = Apple22.valueOf("Winesap");
 		System.out.println("ap contains " + ap);
 
 	}
